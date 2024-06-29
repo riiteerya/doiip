52db38433b866c4233b647233291259b30a6f902
riiteerya <124338509+riiteerya@users.noreply.github.com>
2024-06-29 15:46:53 +0700

    Update rotate_ip.sh

--- changeip/rotate_ip.sh
+++ changeip/rotate_ip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Đường dẫn tới file chứa danh sách IP
-IP_FILE="/path/to/ips.txt"
+# Đường dẫn tới file chứa danh sách IP trên GitHub
+IP_FILE="https://raw.githubusercontent.com/riiteerya/doiip/main/changeip/ips.txt"
 
 # Đọc địa chỉ IP từ danh sách
-IP=$(head -n 1 $IP_FILE)
+IP=$(curl -s $IP_FILE | head -n 1)
 
 # Thay đổi địa chỉ IP cho interface eth0 (thay eth0 nếu cần thiết)
 sudo ifconfig eth0 $IP netmask 255.255.255.0
 
 # Xóa dòng đầu tiên trong file để chuẩn bị cho lần lặp tiếp theo
-sed -i '1d' $IP_FILE
+curl -s $IP_FILE | sed -i '1d'
